8e6b9448fb638b340b52e1071321aff53422294b
cae11cae <cae11cae@naver.com>
2013-09-10 18:56:22 +0900

    Inital commit of original source

---
+++ make.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+#######################################################
+#  PANTECH defined Environment                        #
+#######################################################
+# export TARGET_BUILD_SKY_MODEL_NAME=IM-A750K
+# export TARGET_BUILD_SKY_MODEL_ID=MODEL_EF32K
+# export TARGET_BUILD_SKY_BOARD_VER=EF32K_ES20
+# export TARGET_BUILD_SKY_FIRMWARE_VER=S0832143
+# export TARGET_BUILD_SKY_CUST_INCLUDE=$PWD/include/CUST_SKY.h
+# export TARGET_BUILD_SKY_CUST_INCLUDE_DIR=$PWD/include
+#######################################################
+
+ODIR=../build_ef52k
+image_filename="$ODIR/arch/arm/boot/zImage"
+TOOLCHAIN=../../../linaro/bin/arm-eabi-
+
+export ARCH=arm
+export toolchain=../../../linaro/bin/arm-eabi-
+KERNEL_DEFCONFIG=msm8960_ef52k_tp20_perf_defconfig
+
+if [ ! -d $ODIR ]; then
+  mkdir $ODIR
+  chmod 755 $ODIR
+fi
+
+if [ ! -f $ODIR/.config ]; then
+  echo "load defconfig"
+  make O=$ODIR ARCH=arm CROSS_COMPILE=$TOOLCHAIN $KERNEL_DEFCONFIG 
+fi
+
+if [ "$1" = "" ]; then
+  make CONFIG_NO_ERROR_ON_MISMATCH=y CONFIG_DEBUG_SECTION_MISMATCH=y -j16 O=$ODIR ARCH=arm CROSS_COMPILE=$TOOLCHAIN zImage
+elif [ "$1" == "modules" ]; then
+  make -j16 0=$ODIR ARCH=arm CROSS_COMPILE=$TOOLCAHIN modules
+  cd ..
+  ./getmod.sh
+else
+  make -j16 O=$ODIR ARCH=arm CROSS_COMPILE=$TOOLCHAIN $1 $2 $3
+fi
+
+if [ -f $image_filename ]; then
+  echo "   make boot.img"
+  ./ktmake.sh
+fi
+
+
---
+++ skmake.sh
@@ -0,0 +1,4 @@
+mkbootimg --cmdline " console=NULL,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x3F ehci-hcd.park=3 maxcpus=2 loglevel=0 vmalloc=0x12c00000" --base 0x80200000 --pagesize 2048 --kernel ../build_ef52k/arch/arm/boot/zImage --ramdisk skboot.img-ramdisk.gz  --ramdiskaddr 0x82200000 -o ../skboot.img
+
+
+
